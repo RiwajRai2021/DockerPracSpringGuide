db46517e20c5eecbfedb02bc619fbe101360128a
Riwaj Rai <riwajrai83@gmail.com>
2024-10-12 23:31:59 -0400

    Adding all the essetial files

---
+++ src/test/java/com/example/DockerPracSpringGuides/DockerPracSpringGuidesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DockerPracSpringGuides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerPracSpringGuidesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
